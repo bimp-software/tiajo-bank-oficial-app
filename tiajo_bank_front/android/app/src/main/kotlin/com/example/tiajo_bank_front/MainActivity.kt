845a2dbb089f5e2113ab4a4987523090850190f4
bimp-software <bimp.software@gmail.com>
2026-07-20 17:05:38 -0400

    correccion y mejora

--- tiajo_bank_front/android/app/src/main/kotlin/com/example/tiajo_bank_front/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tiajo_bank_front
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- tiajo_bank_front/lib/core/theme/app_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-final ThemeData appTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-);
